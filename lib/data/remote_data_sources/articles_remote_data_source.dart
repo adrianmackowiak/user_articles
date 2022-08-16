525de9fb214eb85bcd5d6722ef90f07cacd8cbb6
Adrian Maćkowiak <adrianmackowiak1@wp.pl>
2022-08-16 21:06:09 +0200

    add dioError

--- lib/data/remote_data_sources/articles_remote_data_source.dart
+++ lib/data/remote_data_sources/articles_remote_data_source.dart
@@ -21,12 +21,16 @@ class ArticlesMockedDataSource {
 
 class ArticlesRemoteDioDataSource {
   Future<List<Map<String, dynamic>>?> getArticles() async {
-    final response = await Dio().get<List<dynamic>>(
-        'https://my-json-server.typicode.com/adamsmaka/json-demo/articles');
-    final listDynamic = response.data;
-    if (listDynamic == null) {
-      return null;
+    try {
+      final response = await Dio().get<List<dynamic>>(
+          'https://my-json-server.typicode.com/adamsmaka/json-demo/articles');
+      final listDynamic = response.data;
+      if (listDynamic == null) {
+        return null;
+      }
+      return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+    } on DioError catch (error) {
+      throw Exception(error.response?.data ?? "Unknown Error");
     }
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
   }
 }
