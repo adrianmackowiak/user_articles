f8b0abe26fe98776b2c2039855879774a49bc5e4
Adrian Maćkowiak <adrianmackowiak1@wp.pl>
2022-08-17 23:11:29 +0200

    Add jsonSerializable

--- lib/domain/models/author_model.dart
+++ lib/domain/models/author_model.dart
@@ -1,3 +1,11 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:user_articles/domain/models/article_model.dart';
+
+part 'author_model.g.dart';
+
+@JsonSerializable()
 class AuthorModel {
   const AuthorModel({
     required this.id,
@@ -6,18 +14,30 @@ class AuthorModel {
     required this.lastName,
   });
 
+  @JsonKey(name: 'id')
   final int id;
+
+  @JsonKey(name: 'picture')
   final String picture;
+
+  @JsonKey(name: 'first_name')
   final String firstName;
+
+  @JsonKey(name: 'last_name')
   final String lastName;
 
   String get name {
     return '$firstName $lastName';
   }
 
-  AuthorModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        picture = json['picture'],
-        firstName = json['first_name'],
-        lastName = json['last_name'];
+  factory AuthorModel.fromJson(Map<String, dynamic> json) =>
+      _$AuthorModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthorModelToJson(this);
+
+  // AuthorModel.fromJson(Map<String, dynamic> json)
+  //     : id = json['id'],
+  //       picture = json['picture'],
+  //       firstName = json['first_name'],
+  //       lastName = json['last_name'];
 }
